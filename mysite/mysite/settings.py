a15cbd64c7c552a1407723d44bba42913dc970e3
Brian Chow <32953270+khc25@users.noreply.github.com>
2020-10-05 02:24:16 +0800

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,12 +75,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bcggabxf',
+        'USER': 'bcggabxf',
+        'PASSWORD':'sSwTtc4ox8KBe09vrr_-G2ioLeLT-nA1' ,
+        'HOST': 'lallah.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
